b0e4c5c07b0c258b308ebecf7c816d4940f48132
Alessandro Straccia <astraccia@gmail.com>
2025-03-03 09:26:18 -0300

    Feat: first commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# lens_studio_webgl_recording
+# Update the code with your lens IDs:
+
+1. Go to file `src/main.js` and change the lines:
+
+```
+const apiToken = "YOUR_API_TOKEN";
+```
+
+Update YOUR_API_TOKEN with your Lens Studio CameraKit API Token
+
+```
+const lens = await cameraKit.lensRepository.loadLens('your lens ID','your lens folder ID');
+```
+
+Update the lens ID and the lens folder ID with the new lens ID and the new lens folder ID you will find here:
+
+
+
+# To build the project:
+
+Go to the main folder and run the following command to install all the dependencies for the project:
+
+```
+npm i
+```
+
+
+To run the project locally:
+
+```
+npm start
+```
+
+To build the project for publishing:
+
+```
+npm run build
+```
+
+It will build the project in the folder `app`. Simply upload the files in the `app` folder to your web server.
+Important: your webserver should have a SSL certificate (https) in order to run the webApp
+
+
+
---
+++ src/main.js
@@ -0,0 +1,158 @@
+import { bootstrapCameraKit, createMediaStreamSource } from "@snap/camera-kit";
+
+
+let startRecordingButton, videoContainer, stopRecordingButton, downloadButton, shareButton, closeBtn, liveRenderTarget, mediaRecorderOptions, mediaRecorder,downloadUrl;
+let recording_ui;
+
+async function main() {
+
+    init();
+
+    const apiToken = "YOUR_API_TOKEN";
+    const cameraKit = await bootstrapCameraKit({ apiToken });
+
+    liveRenderTarget = document.getElementById("canvas");
+    const session = await cameraKit.createSession({ liveRenderTarget: liveRenderTarget });
+    liveRenderTarget.replaceWith(session.output.live);
+
+    session.events.addEventListener('error', (event) => {
+      if (event.detail.error.name === 'LensExecutionError') {
+        console.log('The current Lens encountered an error and was removed.', event.detail.error);
+      }
+    });
+
+    const stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode:'environment'} });
+    const source = createMediaStreamSource(stream, { cameraType: 'back' });
+    await session.setSource(source);
+
+    const lens = await cameraKit.lensRepository.loadLens('YOUR_LENS_ID','YOUR_GROUP_ID');
+    await session.applyLens(lens);
+
+    session.source.setRenderSize(window.innerWidth,window.innerHeight)
+    await session.play();
+    console.log("Lens rendering has started!");
+
+    recording_ui.classList.add('show')
+}
+
+async function init() {
+
+  videoContainer = document.getElementById("preview")
+  startRecordingButton = document.getElementById("start")
+  stopRecordingButton = document.getElementById("stop")
+  downloadButton = document.getElementById("download")
+  shareButton = document.getElementById("share_btn")
+  closeBtn = document.getElementById("close_btn")
+  recording_ui = document.querySelector("#recording_ui")
+  
+
+  mediaRecorderOptions = { audio: false, video: true, videoBitsPerSecond: 2500000 };
+
+  startRecordingButton.addEventListener("click", () => {
+      startRecordingButton.disabled = true
+      stopRecordingButton.disabled = false
+      downloadButton.disabled = true
+
+      const mediaStream = liveRenderTarget.captureStream(30)
+
+      mediaRecorder = new MediaRecorder(mediaStream, mediaRecorderOptions)
+      
+
+      var chunks = [];
+      mediaRecorder.ondataavailable = function(e) {
+        chunks.push(e.data);
+      };
+
+      mediaRecorder.start();
+
+      
+      mediaRecorder.addEventListener("dataavailable", event => {
+        if (!event.data.size) {
+          console.warn("No recorded data available")
+          return
+        }
+
+        const blob = new Blob([event.data])
+
+        globalThis.blob = blob;
+
+        downloadUrl = window.URL.createObjectURL(blob, {type: 'video/mp4'})
+        downloadButton.disabled = false
+
+        if (document.querySelector('#preview video')) {
+          document.querySelector('#preview video').remove();
+        }
+
+        const video = document.createElement("video");
+        video.setAttribute('autoplay','')
+        video.setAttribute('muted','')
+        video.setAttribute('loop','')
+        video.setAttribute('playsinline','')
+        video.setAttribute('crossorigin','anonymous')
+        const videoSource = document.createElement('source');
+        videoSource.type = 'video/mp4';
+        videoSource.src = URL.createObjectURL(blob);
+        video.append(videoSource);
+        video.play();
+        videoContainer.appendChild(video);
+
+        videoContainer.classList.add('show');
+      })
+
+    })
+
+    stopRecordingButton.addEventListener("click", () => {
+      startRecordingButton.disabled = false
+      stopRecordingButton.disabled = true
+
+      mediaRecorder.stop()
+    })
+
+
+
+
+    downloadButton.addEventListener("click", () => {
+
+      const link = document.createElement("a")
+
+      link.setAttribute("style", "display: none")
+      link.href = downloadUrl
+      link.download = "webar_rec_.mp4"
+      link.click()
+      link.remove()
+    })
+
+    shareButton.addEventListener("click", async () =>  {
+      if (navigator.share) {
+
+        let filesArray = []; 
+        let shareData = null;
+        const file = new File([globalThis.blob], 'webar_rec.mp4', {type: 'video/mp4'});
+
+        filesArray = [file];
+        shareData = { files: filesArray };
+
+        if (navigator.canShare && navigator.canShare(shareData)) {
+          // Adding title afterwards as navigator.canShare just takes files as input
+          shareData.title = 'Lens Studio CameraKit'
+          shareData.text = 'Testing Native Share Functionality.'
+          shareData.url = 'URL'
+          navigator.share(shareData)
+          .then(() => console.log('Share was successful.'))
+          .catch((error) => console.log('Sharing failed', error)); // Can see if aborted here! - "Abort due to cancellation of share."
+      } else {
+          console.log("Your system doesn't support sharing files");
+      }
+
+    }
+        
+    })
+
+    closeBtn.addEventListener("click", () => {
+      videoContainer.classList.remove('show');
+    });
+  
+}
+
+
+document.addEventListener("DOMContentLoaded", main);
